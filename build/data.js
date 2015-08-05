b93aa8ed7011714b30fa06e96a277a09d4b6ea1d
meikidd <meikidd@gmail.com>
2015-08-06 00:58:20 +0800

    initial commit

--- build/data.js
+++ build/data.js
@@ -1,85 +1,75 @@
-export var CODE_LIST = [
-  'ar', 
-  'de', 
-  'en', 
-  'es', 
-  'fr', 
-  'id', 
-  'it', 
-  'ja', 
-  'ko', 
-  'nl', 
-  'pt', 
-  'ru', 
-  'th', 
-  'tr', 
-  'vi', 
-  'zh'
-]
+'use strict';
 
-export var LANGUAGES_LIST = {
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var CODE_LIST = ['ar', 'de', 'en', 'es', 'fr', 'id', 'it', 'ja', 'ko', 'nl', 'pt', 'ru', 'th', 'tr', 'vi', 'zh'];
+
+exports.CODE_LIST = CODE_LIST;
+var LANGUAGES_LIST = {
   ar: {
     name: 'Arabic',
     nativeName: 'اللغة العربية'
-  }, 
+  },
   de: {
     name: 'German',
     nativeName: 'Deutsch'
-  }, 
+  },
   en: {
     name: 'English',
     nativeName: 'English'
-  }, 
+  },
   es: {
     name: 'Spanish',
     nativeName: 'Español'
-  }, 
+  },
   fr: {
     name: 'French',
     nativeName: 'Français'
-  }, 
+  },
   id: {
     name: 'Indonesian',
     nativeName: 'Indonesian'
-  }, 
+  },
   it: {
     name: 'Italian',
     nativeName: 'Italiano'
-  }, 
+  },
   ja: {
     name: 'Japanese',
     nativeName: '日本語'
-  }, 
+  },
   ko: {
     name: 'Korean',
     nativeName: '한국어'
-  }, 
+  },
   nl: {
     name: 'Dutch',
     nativeName: 'nederlands'
-  }, 
+  },
   pt: {
     name: 'Portuguese',
     nativeName: 'Português'
-  }, 
+  },
   ru: {
     name: 'Russian',
     nativeName: 'Русский'
-  }, 
+  },
   th: {
     name: 'Thai',
     nativeName: 'ไทย'
-  }, 
+  },
   tr: {
     name: 'Turkish',
     nativeName: 'Türkçe'
-  }, 
+  },
   vi: {
     name: 'Vietnamese',
     nativeName: 'Việt Nam'
-  }, 
+  },
   zh: {
     name: 'Chinese',
     nativeName: '中文'
   }
-}
+};
+exports.LANGUAGES_LIST = LANGUAGES_LIST;
---
+++ build/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj['default'] = obj; return newObj; } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _data = require('./data');
+
+var DATA = _interopRequireWildcard(_data);
+
+var ISO6391 = (function () {
+  function ISO6391() {
+    _classCallCheck(this, ISO6391);
+  }
+
+  _createClass(ISO6391, null, [{
+    key: 'getName',
+    value: function getName(code) {
+      if (!ISO6391.validate(code)) return '';
+      return DATA.LANGUAGES_LIST[code].name;
+    }
+  }, {
+    key: 'getNativeName',
+    value: function getNativeName(code) {
+      if (!ISO6391.validate(code)) return '';
+      return DATA.LANGUAGES_LIST[code].nativeName;
+    }
+  }, {
+    key: 'getCode',
+    value: function getCode(name) {
+      for (var i = 0; i < DATA.CODE_LIST.length; i++) {
+        var code = DATA.CODE_LIST[i];
+        var language = DATA.LANGUAGES_LIST[code];
+        if (language.name === name || language.nativeName === name) return code;
+      }
+      return '';
+    }
+  }, {
+    key: 'validate',
+    value: function validate(code) {
+      if (DATA.CODE_LIST.indexOf(code) === -1) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }]);
+
+  return ISO6391;
+})();
+
+exports['default'] = ISO6391;
+module.exports = exports['default'];
