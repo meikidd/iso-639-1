fc2775ec80043839d0e485c04f3103d1e5c0608a
meikidd <meikidd@gmail.com>
2015-08-26 16:50:15 +0800

    update data.js

--- build/data.js
+++ build/data.js
@@ -3,29 +3,193 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
-var CODE_LIST = ['ar', 'de', 'en', 'es', 'fr', 'id', 'it', 'ja', 'ko', 'nl', 'pt', 'ru', 'th', 'tr', 'vi', 'zh'];
+var CODE_LIST = ['aa', 'ab', 'ae', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay', 'az', 'ba', 'be', 'bg', 'bh', 'bi', 'bm', 'bn', 'br', 'bs', 'ca', 'ce', 'ch', 'co', 'cr', 'cs', 'cv', 'da', 'de', 'dv', 'dz', 'ee', 'en', 'eo', 'es', 'et', 'eu', 'ff', 'fi', 'fj', 'fr', 'ga', 'gl', 'gn', 'gu', 'id', 'it', 'ja', 'ko', 'nl', 'pt', 'ru', 'th', 'tr', 'vi', 'zh'];
 
 exports.CODE_LIST = CODE_LIST;
 var LANGUAGES_LIST = {
+  aa: {
+    name: 'Afar',
+    nativeName: 'Afaraf'
+  },
+  ab: {
+    name: 'Abkhaz',
+    nativeName: 'аҧсуа бызшәа'
+  },
+  ae: {
+    name: 'Avestan',
+    nativeName: 'avesta'
+  },
+  af: {
+    name: 'Afrikaans',
+    nativeName: 'Afrikaans'
+  },
+  ak: {
+    name: 'Akan',
+    nativeName: 'Akan'
+  },
+  am: {
+    name: 'Amharic',
+    nativeName: 'አማርኛ'
+  },
+  an: {
+    name: 'Aragonese',
+    nativeName: 'aragonés'
+  },
   ar: {
     name: 'Arabic',
     nativeName: 'اللغة العربية'
+  },
+  as: {
+    name: 'Assamese',
+    nativeName: 'অসমীয়া'
+  },
+  av: {
+    name: 'Avaric',
+    nativeName: 'авар мацӀ'
+  },
+  ay: {
+    name: 'Aymara',
+    nativeName: 'aymar aru'
+  },
+  az: {
+    name: 'Azerbaijani',
+    nativeName: 'azərbaycan dili'
+  },
+  ba: {
+    name: 'Bashkir',
+    nativeName: 'башҡорт теле'
+  },
+  be: {
+    name: 'Belarusian',
+    nativeName: 'беларуская мова'
+  },
+  bg: {
+    name: 'Bulgarian',
+    nativeName: 'български език'
+  },
+  bh: {
+    name: 'Bihari',
+    nativeName: 'भोजपुरी'
+  },
+  bi: {
+    name: 'Bislama',
+    nativeName: 'Bislama'
+  },
+  bm: {
+    name: 'Bambara',
+    nativeName: 'bamanankan'
+  },
+  bn: {
+    name: 'Bengali',
+    nativeName: 'বাংলা'
+  },
+  br: {
+    name: 'Breton',
+    nativeName: 'brezhoneg'
+  },
+  bs: {
+    name: 'Bosnian',
+    nativeName: 'bosanski jezik'
+  },
+  ca: {
+    name: 'Catalan',
+    nativeName: 'català'
+  },
+  ce: {
+    name: 'Chechen',
+    nativeName: 'нохчийн мотт'
+  },
+  ch: {
+    name: 'Chamorro',
+    nativeName: 'Chamoru'
+  },
+  co: {
+    name: 'Corsican',
+    nativeName: 'corsu'
+  },
+  cr: {
+    name: 'Cree',
+    nativeName: 'ᓀᐦᐃᔭᐍᐏᐣ'
+  },
+  cs: {
+    name: 'Czech',
+    nativeName: 'čeština'
+  },
+  cv: {
+    name: 'Chuvash',
+    nativeName: 'чӑваш чӗлхи'
+  },
+  da: {
+    name: 'Danish',
+    nativeName: 'dansk'
   },
   de: {
     name: 'German',
     nativeName: 'Deutsch'
   },
+  dv: {
+    name: 'Divehi',
+    nativeName: 'Dhivehi'
+  },
+  dz: {
+    name: 'Dzongkha',
+    nativeName: 'རྫོང་ཁ'
+  },
+  ee: {
+    name: 'Ewe',
+    nativeName: 'Eʋegbe'
+  },
   en: {
     name: 'English',
     nativeName: 'English'
+  },
+  eo: {
+    name: 'Esperanto',
+    nativeName: 'Esperanto'
   },
   es: {
     name: 'Spanish',
     nativeName: 'Español'
   },
+  et: {
+    name: 'Estonian',
+    nativeName: 'eesti'
+  },
+  eu: {
+    name: 'Basque',
+    nativeName: 'euskara'
+  },
+  ff: {
+    name: 'Fula',
+    nativeName: 'Fulfulde'
+  },
+  fi: {
+    name: 'Finnish',
+    nativeName: 'suomi'
+  },
+  fj: {
+    name: 'Fijian',
+    nativeName: 'Vakaviti'
+  },
   fr: {
     name: 'French',
     nativeName: 'Français'
+  },
+  ga: {
+    name: 'Irish',
+    nativeName: 'Gaeilge'
+  },
+  gl: {
+    name: 'Galician',
+    nativeName: 'galego'
+  },
+  gn: {
+    name: 'Guaraní',
+    nativeName: 'Avañe\'ẽ'
+  },
+  gu: {
+    name: 'Gujarati',
+    nativeName: 'ગુજરાતી'
   },
   id: {
     name: 'Indonesian',
