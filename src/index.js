11c7677725c648d3c5154d7814af2609724ab9de
ridgeboy40 <ridgeboy40@gmail.com>
2016-01-26 11:34:32 -0500

    methods getAllCodes(), getAllNames(), and getAllNativeNames()

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default class ISO6391 {
     var list = []
     for (var i = 0; i < codes.length; i++) {
       list.push({
-        code:codes[i], 
+        code:codes[i],
         name: ISO6391.getName(codes[i]),
         nativeName: ISO6391.getNativeName(codes[i])
       })
@@ -16,9 +16,23 @@ export default class ISO6391 {
     if(!ISO6391.validate(code)) return ''
     return DATA.LANGUAGES_LIST[code].name
   }
+  static getAllNames(){
+    var list = [];
+    for( var code in DATA.LANGUAGES_LIST ){
+      list.push(DATA.LANGUAGES_LIST[code].name)
+    }
+    return list;
+  }
   static getNativeName(code) {
     if(!ISO6391.validate(code)) return ''
     return DATA.LANGUAGES_LIST[code].nativeName
+  }
+  static getAllNativeNames(){
+    var list = [];
+    for( var code in DATA.LANGUAGES_LIST ){
+      list.push(DATA.LANGUAGES_LIST[code].nativeName)
+    }
+    return list;
   }
   static getCode(name) {
     for (var i = 0; i < DATA.CODE_LIST.length; i++) {
@@ -27,6 +41,9 @@ export default class ISO6391 {
       if(language.name === name || language.nativeName === name) return code
     }
     return ''
+  }
+  static getAllCodes(){
+    return DATA.CODE_LIST;
   }
   static validate(code) {
     if(DATA.CODE_LIST.indexOf(code) === -1) {
