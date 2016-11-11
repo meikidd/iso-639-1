6e31a2fc4fcfbe77b36da0f97ee6255a97d179e7
nickhsu <nickhsu.tw@gmail.com>
2016-11-11 11:26:38 +0800

    using es6 and refectoring

--- build/index.js
+++ build/index.js
@@ -1,95 +1,99 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _keys = require('babel-runtime/core-js/object/keys');
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj['default'] = obj; return newObj; } }
+var _keys2 = _interopRequireDefault(_keys);
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+var _values = require('babel-runtime/core-js/object/values');
+
+var _values2 = _interopRequireDefault(_values);
+
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
+var _slicedToArray2 = require('babel-runtime/helpers/slicedToArray');
+
+var _slicedToArray3 = _interopRequireDefault(_slicedToArray2);
+
+var _entries = require('babel-runtime/core-js/object/entries');
+
+var _entries2 = _interopRequireDefault(_entries);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
 var _data = require('./data');
 
-var DATA = _interopRequireWildcard(_data);
+var _data2 = _interopRequireDefault(_data);
 
-var ISO6391 = (function () {
-  function ISO6391() {
-    _classCallCheck(this, ISO6391);
-  }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-  _createClass(ISO6391, null, [{
-    key: 'getLanguages',
-    value: function getLanguages(codes) {
-      var list = [];
-      for (var i = 0; i < codes.length; i++) {
-        list.push({
-          code: codes[i],
-          name: ISO6391.getName(codes[i]),
-          nativeName: ISO6391.getNativeName(codes[i])
-        });
-      }
-      return list;
-    }
-  }, {
-    key: 'getName',
-    value: function getName(code) {
-      if (!ISO6391.validate(code)) return '';
-      return DATA.LANGUAGES_LIST[code].name;
-    }
-  }, {
-    key: 'getAllNames',
-    value: function getAllNames() {
-      var list = [];
-      for (var code in DATA.LANGUAGES_LIST) {
-        list.push(DATA.LANGUAGES_LIST[code].name);
-      }
-      return list;
-    }
-  }, {
-    key: 'getNativeName',
-    value: function getNativeName(code) {
-      if (!ISO6391.validate(code)) return '';
-      return DATA.LANGUAGES_LIST[code].nativeName;
-    }
-  }, {
-    key: 'getAllNativeNames',
-    value: function getAllNativeNames() {
-      var list = [];
-      for (var code in DATA.LANGUAGES_LIST) {
-        list.push(DATA.LANGUAGES_LIST[code].nativeName);
-      }
-      return list;
-    }
-  }, {
-    key: 'getCode',
-    value: function getCode(name) {
-      for (var i = 0; i < DATA.CODE_LIST.length; i++) {
-        var code = DATA.CODE_LIST[i];
-        var language = DATA.LANGUAGES_LIST[code];
-        if (language.name === name || language.nativeName === name) return code;
-      }
-      return '';
-    }
-  }, {
-    key: 'getAllCodes',
-    value: function getAllCodes() {
-      return DATA.CODE_LIST;
-    }
-  }, {
-    key: 'validate',
-    value: function validate(code) {
-      if (DATA.CODE_LIST.indexOf(code) === -1) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  }]);
+var ISO6391 = function ISO6391() {
+  (0, _classCallCheck3.default)(this, ISO6391);
+};
 
-  return ISO6391;
-})();
+ISO6391.getLanguages = function (codes) {
+  return (0, _entries2.default)(_data2.default).filter(function (_ref) {
+    var _ref2 = (0, _slicedToArray3.default)(_ref, 2),
+        code = _ref2[0],
+        language = _ref2[1];
 
-exports['default'] = ISO6391;
-module.exports = exports['default'];
+    return codes.includes(code);
+  }).map(function (_ref3) {
+    var _ref4 = (0, _slicedToArray3.default)(_ref3, 2),
+        code = _ref4[0],
+        language = _ref4[1];
+
+    return (0, _extends3.default)({}, language, { code: code });
+  });
+};
+
+ISO6391.getName = function (code) {
+  return ISO6391.validate(code) ? _data2.default[code].name : '';
+};
+
+ISO6391.getAllNames = function () {
+  return (0, _values2.default)(_data2.default).map(function (l) {
+    return l.name;
+  });
+};
+
+ISO6391.getNativeName = function (code) {
+  return ISO6391.validate(code) ? _data2.default[code].nativeName : '';
+};
+
+ISO6391.getAllNativeNames = function () {
+  return (0, _values2.default)(_data2.default).map(function (l) {
+    return l.nativeName;
+  });
+};
+
+ISO6391.getCode = function (name) {
+  var _Object$entries$find = (0, _entries2.default)(_data2.default).find(function (_ref5) {
+    var _ref6 = (0, _slicedToArray3.default)(_ref5, 2),
+        code = _ref6[0],
+        language = _ref6[1];
+
+    return language.name === name || language.nativeName === name;
+  }),
+      _Object$entries$find2 = (0, _slicedToArray3.default)(_Object$entries$find, 1),
+      code = _Object$entries$find2[0];
+
+  return code === undefined ? '' : code;
+};
+
+ISO6391.getAllCodes = function () {
+  return (0, _keys2.default)(_data2.default);
+};
+
+ISO6391.validate = function (code) {
+  return _data2.default[code] !== undefined;
+};
+
+exports.default = ISO6391;
--- src/index.js
+++ src/index.js
@@ -1,55 +1,28 @@
-import * as DATA from './data'
+import LANGUAGES_LIST from './data';
 
 export default class ISO6391 {
-  static getLanguages(codes) {
-    var list = []
-    for (var i = 0; i < codes.length; i++) {
-      list.push({
-        code:codes[i],
-        name: ISO6391.getName(codes[i]),
-        nativeName: ISO6391.getNativeName(codes[i])
-      })
-    }
-    return list
+  static getLanguages = (codes) => (
+    Object.entries(LANGUAGES_LIST)
+      .filter(([code, language]) => codes.includes(code))
+      .map(([code, language]) => ({ ...language, code }))
+  )
+
+  static getName = (code) => ISO6391.validate(code) ? LANGUAGES_LIST[code].name : ''
+
+  static getAllNames = () => Object.values(LANGUAGES_LIST).map(l => l.name)
+
+  static getNativeName = (code) => ISO6391.validate(code) ? LANGUAGES_LIST[code].nativeName : ''
+
+  static getAllNativeNames = () => Object.values(LANGUAGES_LIST).map(l => l.nativeName)
+
+  static getCode = (name) => {
+    const [code] = Object.entries(LANGUAGES_LIST)
+      .find(([code, language]) => language.name === name || language.nativeName === name)
+
+    return code === undefined ? '' : code
   }
-  static getName(code) {
-    if(!ISO6391.validate(code)) return ''
-    return DATA.LANGUAGES_LIST[code].name
-  }
-  static getAllNames(){
-    var list = [];
-    for( var code in DATA.LANGUAGES_LIST ){
-      list.push(DATA.LANGUAGES_LIST[code].name)
-    }
-    return list;
-  }
-  static getNativeName(code) {
-    if(!ISO6391.validate(code)) return ''
-    return DATA.LANGUAGES_LIST[code].nativeName
-  }
-  static getAllNativeNames(){
-    var list = [];
-    for( var code in DATA.LANGUAGES_LIST ){
-      list.push(DATA.LANGUAGES_LIST[code].nativeName)
-    }
-    return list;
-  }
-  static getCode(name) {
-    for (var i = 0; i < DATA.CODE_LIST.length; i++) {
-      var code = DATA.CODE_LIST[i]
-      var language = DATA.LANGUAGES_LIST[code]
-      if(language.name === name || language.nativeName === name) return code
-    }
-    return ''
-  }
-  static getAllCodes(){
-    return DATA.CODE_LIST;
-  }
-  static validate(code) {
-    if(DATA.CODE_LIST.indexOf(code) === -1) {
-      return false
-    } else {
-      return true
-    }
-  }
+
+  static getAllCodes = () => Object.keys(LANGUAGES_LIST)
+
+  static validate = (code) => LANGUAGES_LIST[code] !== undefined
 }
